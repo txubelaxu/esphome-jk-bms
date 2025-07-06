79a705a69f7983fd460cc0cb469eb0f831a8e10a
rabbit3dcustom <rabbit3dcustom@WIN10PROGRAMMIN>
2025-07-06 17:18:10 +0200

    rollback

--- components/jk_rs485_bms/jk_rs485_bms.cpp
+++ components/jk_rs485_bms/jk_rs485_bms.cpp
@@ -679,9 +679,6 @@ void JkRS485Bms::on_jk_rs485_sniffer_data(const uint8_t &origin_address, const u
         ESP_LOGD(TAG, "  %s", format_hex_pretty(&data.front(), 150).c_str());
     }
 
-    ESP_LOGD(TAG, "this->cell_count_real_sensor_->state: %d ", this->cell_count_real_sensor_->state);
-    ESP_LOGD(TAG, "this->cell_count_settings_number_->state: %d ", this->cell_count_settings_number_->state);
-
     if (this->cell_count_real_sensor_->state > 0 && this->cell_count_settings_number_->state > 0) {
       this->reset_status_online_tracker_();
     } else {

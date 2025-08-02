426176bd8cd44fa7c52e925279fc9289df7c3ba9
rabbit3dcustom <rabbit3dcustom@gmail.com>
2025-08-02 07:57:17 +0200

    deprecations 04

--- components/jk_rs485_bms/switch/__init__.py
+++ components/jk_rs485_bms/switch/__init__.py
@@ -1,14 +1,18 @@
-
 import esphome.codegen as cg
-from esphome.components.switch import Switch, switch_schema, register_switch
+from esphome.components import switch
 import esphome.config_validation as cv
 from esphome.const import CONF_ICON, CONF_ID
 
 from .. import CONF_JK_RS485_BMS_ID, JK_RS485_BMS_COMPONENT_SCHEMA, jk_rs485_bms_ns
-from ..const import *
+from ..const import (
+    CONF_BALANCING, CONF_PRECHARGING, CONF_CHARGING, CONF_DISCHARGING, CONF_DISPLAY_ALWAYS_ON, CONF_EMERGENCY, CONF_HEATING, CONF_CHARGING_FLOAT_MODE, 
+    CONF_SMART_SLEEP_ON, CONF_DISABLE_PCL_MODULE,CONF_DISABLE_TEMPERATURE_SENSORS, CONF_TIMED_STORED_DATA,
+    CONF_GPS_HEARTBEAT,CONF_PORT_SELECTION,CONF_SPECIAL_CHARGER
+)
 
 DEPENDENCIES = ["jk_rs485_bms"]
-CODEOWNERS = ["@syssi", "@txubelaxu"]
+
+CODEOWNERS = ["@syssi","@txubelaxu"]
 
 ICON_CHARGING = "mdi:battery-charging-50"
 ICON_DISCHARGING = "mdi:battery-charging-50"
@@ -23,50 +27,129 @@ ICON_CHARGING_FLOAT_MODE = "mdi:battery-charging-80"
 ICON_DISPLAY_ALWAYS_ON = "mdi:television"
 
 SWITCHES = {
-    CONF_CHARGING: [0x0070, 0x10, 0x04],
-    CONF_DISCHARGING: [0x0074, 0x10, 0x04],
-    CONF_BALANCING: [0x0078, 0x10, 0x04],
-    CONF_HEATING: [0x0014, 0x11, 0x02],
-    CONF_DISABLE_TEMPERATURE_SENSORS: [0x0014, 0x11, 0x02],
-    CONF_GPS_HEARTBEAT: [0x0014, 0x11, 0x02],
-    CONF_PORT_SELECTION: [0x0014, 0x11, 0x02],
-    CONF_DISPLAY_ALWAYS_ON: [0x0014, 0x11, 0x02],
-    CONF_SPECIAL_CHARGER: [0x0014, 0x11, 0x02],
-    CONF_SMART_SLEEP_ON: [0x0014, 0x11, 0x02],
-    CONF_DISABLE_PCL_MODULE: [0x0014, 0x11, 0x02],
-    CONF_TIMED_STORED_DATA: [0x0014, 0x11, 0x02],
-    CONF_CHARGING_FLOAT_MODE: [0x0014, 0x11, 0x02],
-    CONF_PRECHARGING: [0x0000, 0x00, 0x00],
-    CONF_EMERGENCY: [0x0000, 0x00, 0x00],
+    CONF_CHARGING:                            [0x0070,0x10,0x04],                        #0x1000 -> 0x0078
+    CONF_DISCHARGING:                         [0x0074,0x10,0x04],                        #02.10.10.78.00.02.   04.  00.00.00.00.37.A9
+    CONF_BALANCING:                           [0x0078,0x10,0x04],                       #02.10.10.78.00.02.   04.  00.00.00.01.F6.69.
+
+    CONF_HEATING:                             [0x0014,0x11,0x02],  
+    CONF_DISABLE_TEMPERATURE_SENSORS:         [0x0014,0x11,0x02],     #0x1000 -> 0x0114
+    CONF_GPS_HEARTBEAT:                       [0x0014,0x11,0x02],                   #DISPLAY ALWAYS ON  (0x0114)
+    CONF_PORT_SELECTION:                      [0x0014,0x11,0x02],                  #02.10.11.14.00.01.   02.  02.00.   B1.D5.   OFF
+    CONF_DISPLAY_ALWAYS_ON:                   [0x0014,0x11,0x02],               #02.10.11.14.00.01.   02.  02.10.   B0.19.   ON   0000 0010 0001 0000
+    CONF_SPECIAL_CHARGER:                     [0x0014,0x11,0x02],                  #SMART SLEEP ON
+    CONF_SMART_SLEEP_ON:                      [0x0014,0x11,0x02],                  #02.10.11.14.00.01.   02.  00.10.   B1.79.
+    CONF_DISABLE_PCL_MODULE:                  [0x0014,0x11,0x02],              
+    CONF_TIMED_STORED_DATA:                   [0x0014,0x11,0x02],              
+    CONF_CHARGING_FLOAT_MODE:                 [0x0014,0x11,0x02],             #02.10.11.14.00.01.   02.  02.10.   B0.19.
+
+    CONF_PRECHARGING:                         [0x0000,0x00,0x00],                     #02.10.10.84.00.02.04.00.00.0D.70.3D.CC.
+    CONF_EMERGENCY:                           [0x0000,0x00,0x00],
+  
+    
+    
+    
+    
 }
 
-JkRS485BmsSwitch = jk_rs485_bms_ns.class_("JkRS485BmsSwitch", Switch, cg.Component)
+JkRS485BmsSwitch = jk_rs485_bms_ns.class_("JkRS485BmsSwitch", switch.Switch, cg.Component)
 
-def make_schema(conf, icon):
-    return switch_schema({
-        cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
-        cv.Optional(CONF_ICON, default=icon): cv.icon,
-    }).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = JK_RS485_BMS_COMPONENT_SCHEMA.extend(
+    {
+        cv.Optional(CONF_PRECHARGING): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_CHARGING): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),
+        cv.Optional(CONF_CHARGING): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_CHARGING): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),
+        cv.Optional(CONF_DISCHARGING): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_DISCHARGING): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),
+         cv.Optional(CONF_BALANCING): switch.SWITCH_SCHEMA.extend(
+             {
+                 cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                 cv.Optional(CONF_ICON, default=ICON_BALANCING): cv.icon,
+             }
+         ).extend(cv.COMPONENT_SCHEMA),
+        cv.Optional(CONF_EMERGENCY): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_EMERGENCY): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),  
+        cv.Optional(CONF_HEATING): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_HEATING): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),   
+        cv.Optional(CONF_DISABLE_TEMPERATURE_SENSORS): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(
+                    CONF_ICON, default=ICON_DISABLE_TEMPERATURE_SENSORS
+                ): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),                    
+         cv.Optional(CONF_DISPLAY_ALWAYS_ON): switch.SWITCH_SCHEMA.extend(
+             {
+                 cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                 cv.Optional(CONF_ICON, default=ICON_DISPLAY_ALWAYS_ON): cv.icon,
+             }
+         ).extend(cv.COMPONENT_SCHEMA),    
+        cv.Optional(CONF_SMART_SLEEP_ON): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_SMART_SLEEP_ON): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),    
+        cv.Optional(CONF_TIMED_STORED_DATA): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_TIMED_STORED_DATA): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),             
+        cv.Optional(CONF_CHARGING_FLOAT_MODE): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_CHARGING_FLOAT_MODE): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),  
+        cv.Optional(CONF_DISABLE_PCL_MODULE): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_DISABLE_PCL_MODULE): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),    
+        cv.Optional(CONF_GPS_HEARTBEAT): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_DISABLE_PCL_MODULE): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),  
+        cv.Optional(CONF_PORT_SELECTION): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_DISABLE_PCL_MODULE): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA), 
+        cv.Optional(CONF_SPECIAL_CHARGER): switch.SWITCH_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(JkRS485BmsSwitch),
+                cv.Optional(CONF_ICON, default=ICON_DISABLE_PCL_MODULE): cv.icon,
+            }
+        ).extend(cv.COMPONENT_SCHEMA),         
+    }
+)
 
-CONFIG_SCHEMA = JK_RS485_BMS_COMPONENT_SCHEMA.extend({
-    cv.Optional(k): make_schema(k, icon) for k, icon in {
-        CONF_PRECHARGING: ICON_CHARGING,
-        CONF_CHARGING: ICON_CHARGING,
-        CONF_DISCHARGING: ICON_DISCHARGING,
-        CONF_BALANCING: ICON_BALANCING,
-        CONF_EMERGENCY: ICON_EMERGENCY,
-        CONF_HEATING: ICON_HEATING,
-        CONF_DISABLE_TEMPERATURE_SENSORS: ICON_DISABLE_TEMPERATURE_SENSORS,
-        CONF_DISPLAY_ALWAYS_ON: ICON_DISPLAY_ALWAYS_ON,
-        CONF_SMART_SLEEP_ON: ICON_SMART_SLEEP_ON,
-        CONF_TIMED_STORED_DATA: ICON_TIMED_STORED_DATA,
-        CONF_CHARGING_FLOAT_MODE: ICON_CHARGING_FLOAT_MODE,
-        CONF_DISABLE_PCL_MODULE: ICON_DISABLE_PCL_MODULE,
-        CONF_GPS_HEARTBEAT: ICON_DISABLE_PCL_MODULE,
-        CONF_PORT_SELECTION: ICON_DISABLE_PCL_MODULE,
-        CONF_SPECIAL_CHARGER: ICON_DISABLE_PCL_MODULE,
-    }.items()
-})
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_JK_RS485_BMS_ID])
@@ -75,7 +158,7 @@ async def to_code(config):
             conf = config[key]
             var = cg.new_Pvariable(conf[CONF_ID])
             await cg.register_component(var, conf)
-            await register_switch(var, conf)
+            await switch.register_switch(var, conf)
             cg.add(getattr(hub, f"set_{key}_switch")(var))
             cg.add(var.set_parent(hub))
             cg.add(var.set_register_address(param_config[0]))
--- components/jk_rs485_bms/text_sensor.py
+++ components/jk_rs485_bms/text_sensor.py
@@ -1,48 +1,176 @@
-
 import esphome.codegen as cg
-from esphome.components.text_sensor import TextSensor, text_sensor_schema, register_text_sensor
+from esphome.components import text_sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_ICON, CONF_ID, ICON_EMPTY, ICON_TIMELAPSE
+from esphome.const import CONF_ICON, CONF_ID, CONF_PASSWORD, ICON_EMPTY, ICON_TIMELAPSE
 
 from . import CONF_JK_RS485_BMS_ID, JK_RS485_BMS_COMPONENT_SCHEMA
 
 DEPENDENCIES = ["jk_rs485_bms"]
-CODEOWNERS = ["@syssi", "@txubelaxu"]
 
+CODEOWNERS = ["@syssi","@txubelaxu"]
+
+CONF_BATTERY_TYPE = "battery_type"
 CONF_ERRORS = "errors"
 CONF_OPERATION_STATUS = "operation_status"
-CONF_BATTERY_TYPE = "battery_type"
+CONF_DEVICE_TYPE = "device_type"
+CONF_SOFTWARE_VERSION = "software_version"
+CONF_MANUFACTURER = "manufacturer"
 CONF_TOTAL_RUNTIME_FORMATTED = "total_runtime_formatted"
-CONF_INFO_VENDORID = "info_vendorid"
-CONF_INFO_HARDWARE_VERSION = "info_hardware_version"
-CONF_INFO_SOFTWARE_VERSION = "info_software_version"
-CONF_INFO_DEVICE_NAME = "info_device_name"
-CONF_INFO_DEVICE_PASSWORD = "info_device_password"
 CONF_NETWORK_NODES_AVAILABLE = "network_nodes_available"
-CONF_INFO_DEVICE_SERIAL_NUMBER = "info_device_serial_number"
-CONF_INFO_DEVICE_SETUP_PASSCODE = "info_device_setup_passcode"
+
+CONF_INFO_VENDORID = "info_vendorid"
+CONF_INFO_HARDWARE_VERSION = "info_hardware_version";
+CONF_INFO_SOFTWARE_VERSION = "info_software_version";
+CONF_INFO_DEVICE_NAME = "info_device_name";
+CONF_INFO_DEVICE_PASSWORD = "info_device_password";
+CONF_INFO_DEVICE_SERIAL_NUMBER = "info_device_serial_number";
+CONF_INFO_DEVICE_SETUP_PASSCODE = "info_device_setup_passcode";
+
+ICON_BATTERY_TYPE = "mdi:car-battery"
+ICON_ERRORS = "mdi:alert-circle-outline"
+ICON_OPERATION_MODE = "mdi:heart-pulse"
+ICON_PASSWORD = "mdi:lock-outline"
+ICON_NETWORK = "mdi:lan"
+ICON_OPERATION_STATUS = "mdi:heart-pulse"
+ICON_INFORMATION = "mdi:information"
 
 TEXT_SENSORS = [
-    CONF_ERRORS,
     CONF_OPERATION_STATUS,
+    CONF_ERRORS,
     CONF_BATTERY_TYPE,
+    CONF_PASSWORD,
+    CONF_DEVICE_TYPE,
+    CONF_SOFTWARE_VERSION,
+    CONF_MANUFACTURER,
     CONF_TOTAL_RUNTIME_FORMATTED,
+    CONF_NETWORK_NODES_AVAILABLE,
+
     CONF_INFO_VENDORID,
     CONF_INFO_HARDWARE_VERSION,
     CONF_INFO_SOFTWARE_VERSION,
     CONF_INFO_DEVICE_NAME,
     CONF_INFO_DEVICE_PASSWORD,
-    CONF_NETWORK_NODES_AVAILABLE,
-    CONF_INFO_DEVICE_SERIAL_NUMBER,
-    CONF_INFO_DEVICE_SETUP_PASSCODE,
+    CONF_INFO_DEVICE_SERIAL_NUMBER,   
+    CONF_INFO_DEVICE_SETUP_PASSCODE, 
 ]
 
-CONFIG_SCHEMA = JK_RS485_BMS_COMPONENT_SCHEMA.extend({
-    cv.Optional(sensor): text_sensor_schema({
-        cv.GenerateID(): cv.declare_id(TextSensor),
-        cv.Optional(CONF_ICON, default=ICON_EMPTY): cv.icon,
-    }) for sensor in TEXT_SENSORS
-})
+CONFIG_SCHEMA = JK_RS485_BMS_COMPONENT_SCHEMA.extend(
+    {
+        cv.Optional(CONF_OPERATION_STATUS): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_OPERATION_MODE): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_ERRORS): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_ERRORS): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_BATTERY_TYPE): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_BATTERY_TYPE): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_PASSWORD): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_PASSWORD): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_DEVICE_TYPE): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_EMPTY): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_SOFTWARE_VERSION): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_EMPTY): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_MANUFACTURER): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_EMPTY): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_TOTAL_RUNTIME_FORMATTED): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_TIMELAPSE): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_NETWORK_NODES_AVAILABLE): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_NETWORK): cv.icon,
+            }
+        ),    
+        cv.Optional(
+            CONF_INFO_VENDORID
+        ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_INFORMATION): cv.icon,
+            }
+        ),
+        cv.Optional(
+            CONF_INFO_HARDWARE_VERSION
+        ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_INFORMATION): cv.icon,
+            }
+        ),
+        cv.Optional(
+            CONF_INFO_SOFTWARE_VERSION
+        ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_INFORMATION): cv.icon,
+            }
+        ),
+        cv.Optional(
+            CONF_INFO_DEVICE_NAME
+        ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_INFORMATION): cv.icon,
+            }
+        ),
+        cv.Optional(
+            CONF_INFO_DEVICE_PASSWORD
+        ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_INFORMATION): cv.icon,
+            }
+        ),   
+        cv.Optional(
+            CONF_INFO_DEVICE_SERIAL_NUMBER
+        ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_INFORMATION): cv.icon,
+            }
+        ),                    
+        cv.Optional(
+            CONF_INFO_DEVICE_SETUP_PASSCODE
+        ): text_sensor.TEXT_SENSOR_SCHEMA.extend(
+            {
+                cv.GenerateID(): cv.declare_id(text_sensor.TextSensor),
+                cv.Optional(CONF_ICON, default=ICON_INFORMATION): cv.icon,
+            }
+        ),   
+
+        
+    }
+)
+
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_JK_RS485_BMS_ID])
@@ -50,6 +178,5 @@ async def to_code(config):
         if key in config:
             conf = config[key]
             sens = cg.new_Pvariable(conf[CONF_ID])
-            await register_text_sensor(sens, conf)
-            cg.add(sens.set_parent(hub))
+            await text_sensor.register_text_sensor(sens, conf)
             cg.add(getattr(hub, f"set_{key}_text_sensor")(sens))
